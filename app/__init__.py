29df254a048211e37f4dd463341f9529bdd761ce
mazenyousryy <mazenyyousryy@gmail.com>
2023-04-21 18:55:35 +0200

    Adding Reset Password

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 # __name__ -> name of the module (app.py)
@@ -16,6 +17,13 @@ login_manager = LoginManager(app)
 
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "parla.chatbot.eg@gmail.com"
+app.config['MAIL_PASSWORD'] = "yjxtaarkuljecebg"
+mail = Mail(app)
 
 from app import routes
 
